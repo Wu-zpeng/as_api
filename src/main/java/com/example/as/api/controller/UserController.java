d4587a49f33defc5ce14ebc30e10fdd361990a96
Wu-zpeng <56808242+Wu-zpeng@users.noreply.github.com>
2021-08-02 16:26:36 +0800

    1、在pom.xml添加了swagger2的依赖 2、创建了SwaggerConfig配置类 3、在AsApiApplication上添加了可使用swagger2 4、给UserController添加了swagger的使用

--- src/main/java/com/example/as/api/controller/UserController.java
+++ src/main/java/com/example/as/api/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.as.api.controller;
 
 import com.example.as.api.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/user")
+@Api(tags = {"Account"})
 public class UserController {
     @Autowired
     private UserService mUserService;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
+    @ApiOperation(value = "注册")
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public Object registration(@RequestParam(value = "userName") String userName
-            , @RequestParam(value = "password") String password
-            , @RequestParam(value = "imoocId") String imoocId
-            , @RequestParam(value = "orderId") String orderId) {
+    public Object registration(@RequestParam(value = "userName") @ApiParam("账号或手机号") String userName
+            , @RequestParam(value = "password") @ApiParam("密码") String password
+            , @RequestParam(value = "imoocId") @ApiParam("慕课网用户ID") String imoocId
+            , @RequestParam(value = "orderId") @ApiParam("订单号") String orderId) {
         mUserService.addUser(userName, bCryptPasswordEncoder.encode(password), imoocId, orderId);
         //测试描述
         return "registration success.";
